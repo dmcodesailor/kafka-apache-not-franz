6c1a4315930ab3c8472a8679d6460342c7a20bc9
Brian.Lanham <brian.lanham@advance-auto.com>
2019-03-26 14:39:37 -0400

    Renamed for public consumption;

--- KafkaConsumer/src/main/java/com/rvnug/kafkaconsumer/KafkaConsumer.java
+++ KafkaConsumer/src/main/java/com/rvnug/kafkaconsumer/KafkaConsumer.java
@@ -1,9 +1,8 @@
-package com.aap.kafkaconsumer;
+package com.rvnug.kafkaconsumer;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -11,16 +10,16 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
-public class AapKafkaConsumer {
-    private final KafkaConsumer<Integer, String> consumer;
+public class KafkaConsumer {
+    private final org.apache.kafka.clients.consumer.KafkaConsumer consumer;
     private final String topic;
 
     public static void main(String[] args) {
-        AapKafkaConsumer thisConsumer = new AapKafkaConsumer("exoplanet-data-etl");
+        KafkaConsumer thisConsumer = new KafkaConsumer("exoplanet-data-etl");
         thisConsumer.doIt();
     }
 
-    public AapKafkaConsumer(String topic) {
+    public KafkaConsumer(String topic) {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "exoplanet-data-consumer");
@@ -29,7 +28,7 @@ public class AapKafkaConsumer {
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        consumer = new KafkaConsumer<Integer, String>(props);
+        consumer = new org.apache.kafka.clients.consumer.KafkaConsumer(props);
         this.topic = topic;
     }
 
@@ -38,7 +37,7 @@ public class AapKafkaConsumer {
         try {
             while (true) {
                 ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofSeconds(1));
-                System.out.println("AapKafkaConsumer::doIt::" + records.count());
+                System.out.println("KafkaConsumer::doIt::" + records.count());
                 for (ConsumerRecord<Integer, String> record : records) {
                     System.out.println("Consumed record " + record.key() + " from Parition " + record.partition() + " at Offset " + record.offset());
                 }
